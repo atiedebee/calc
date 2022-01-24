7e80960f5f7b67de4a59ccb24fe3f103ef7eae6e
atiedebee <atiedebee@gmail.com>
2022-01-24 16:57:20 +0100

    added some best-practices to the headers

--- src/converters.h
+++ src/converters.h
@@ -1,5 +1,6 @@
-// CONVERTERS.H //
-
+#define CONVERTERS_H
+#ifdef CONVERTERS_H
 struct statement* stringToStatement(char* input, int MALLOC_SIZE);
 
 double calculate(struct statement* statement);
+#endif
--- src/input.h
+++ src/input.h
@@ -1,1 +1,6 @@
+#define INPUT_H
+#ifdef INPUT_H
+
 char* readData(void);
+
+#endif
